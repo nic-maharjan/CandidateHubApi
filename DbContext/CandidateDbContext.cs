bff0c1538ea533065ac4e7b2b0d9e87c410b42cf
Nikesh Maharjan <mhrjnn.nickeshh@gmail.com>
2024-10-11 21:30:36 +0545

    added DbContext class file and candidates model class.

---
+++ DbContext/CandidateDbContext.cs
@@ -0,0 +1,19 @@
+﻿using static CandidateHubApi.Model.Candidates;
+
+
+namespace CandidateHubApi.DbContext
+{
+	using Microsoft.EntityFrameworkCore;
+	public class CandidateDbContext : DbContext
+	{
+		public CandidateDbContext(DbContextOptions<CandidateDbContext> options) : base(options) { }
+
+		public DbSet<Candidate> Candidates { get; set; }
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			modelBuilder.Entity<Candidate>()
+				.HasKey(c => c.Email);
+		}
+	}
+}
---
+++ Model/Candidates.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CandidateHubApi.Model
+{
+	public class Candidates
+	{
+		public class Candidate
+		{
+			public int Id { get; set; }
+
+			[Required(ErrorMessage = "First name is required.")]
+			public string FirstName { get; set; }
+
+			[Required(ErrorMessage = "Last name is required.")]
+			public string LastName { get; set; }
+
+			[Required(ErrorMessage = "Phone number is required.")]
+			[Phone(ErrorMessage = "Invalid phone number format.")]
+			public string PhoneNumber { get; set; }
+
+			[Required(ErrorMessage = "Email is required.")]
+			[EmailAddress(ErrorMessage = "Invalid email address format.")]
+			public string Email { get; set; }
+
+			public string CallTimeInterval { get; set; }
+			public string LinkedInProfileUrl { get; set; }
+			public string GitHubProfileUrl { get; set; }
+
+			[Required(ErrorMessage = "Comment is required.")]
+			public string Comment { get; set; }
+		}
+	}
+}
